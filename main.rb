acf9a00c1f32ae0b76af03bc0a1f66c278a19f0d
Wesley Ellis <tahnok@gmail.com>
2013-09-24 20:28:52 -0400

    password protect if user and password

--- main.rb
+++ main.rb
@@ -2,8 +2,23 @@ require 'sinatra'
 require 'json'
 require 'cgi'
 
+helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @config = JSON.parse(File.read("config.json"))
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == [@config['user'], @config['password']]
+  end
+end
+
 get '/' do
   @config = JSON.parse(File.read("config.json"))
+  protected! if @config['user'] && @config['password']
   @posts = JSON.parse(File.read("posts.json"))
   @posts.each do |post|
     post['body'] = CGI.unescapeHTML(post['body'])
